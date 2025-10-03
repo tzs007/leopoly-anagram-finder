526eaa3a6c15a901a19abd1b4d59951bea4cff0d
Zsolt Takács <tzsbmo@gmail.com>
2025-10-03 13:48:08 +0200

    button hover fixed

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -5,7 +5,7 @@ type Props = React.ComponentProps<"button">;
 export const Button: React.FC<Props> = ({ className, children, ...rest }) => {
   return (
     <button
-      className={`py-3 px-6 bg-green-900 rounded-full w-full md:w-auto ${className}`}
+      className={`py-3 px-6 bg-green-900 rounded-full w-full md:w-auto hover:bg-green-600 text-black cursor-pointer ${className}`}
       {...rest}
     >
       {children}
